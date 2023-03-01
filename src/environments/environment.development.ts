424a59dd4dba21a5963244db6b289bd64242907f
wagblin3 <wagblin3@gmail.com>
2023-03-01 14:45:25 +0100

    1er test github action

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,5 +4,5 @@ export const environment = {
     apiKey_apiTmdb: '267af5120325d394d4ce784b430c2043',
     // base_url_apiBack: 'https://redline.fr.nf/api/v1',
     base_url_apiBack: 'http://localhost:8080/api',
-    url_apiUser: 'http://localhost:8081/login',
+    url_apiUser: 'http://localhost:8082/login',
 };

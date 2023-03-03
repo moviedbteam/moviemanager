f357c4166bd34d7c401a842b49f05c32ae413c8b
wagblin3 <wagblin3@gmail.com>
2023-03-03 19:44:07 +0100

    mise à jour env non prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,9 @@ export const environment = {
     production: false,
     base_url_apiTmdb : 'https://api.themoviedb.org/3',
     apiKey_apiTmdb: '267af5120325d394d4ce784b430c2043',
-    base_url_apiBack: 'https://redline.fr.nf/api/v1',
-    // base_url_apiBack: 'http://localhost:8080/api/v1',
+    // base_url_apiBack: 'https://redline.fr.nf/api/v1',
+    base_url_apiBack: 'http://localhost:8080/api/v1',
+    // url_apiUser: 'https://redline.fr.nf/login',
+    url_apiUser: 'http://localhost:8082/login',
 };
 

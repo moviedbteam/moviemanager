9c18c127ab44a738fc66e35acd5ae2fd8f1f8e74
wagblin3 <wagblin3@gmail.com>
2023-03-16 22:21:20 +0100

    discover : &vote_average.gte=8&vote_count.gte=1000

--- src/app/services/tv.service.ts
+++ src/app/services/tv.service.ts
@@ -35,6 +35,8 @@ export class TvService {
     let params = new HttpParams()
     .set('api_key', apiKey)
     .set('language', 'fr')
+    .set('vote_average.gte', '8')
+    .set('vote_count.gte', '1000')
     .set('page', this.indexPage);
 
     console.log (urlApi+'?api_key='+apiKey+'&language=fr&page=1');
